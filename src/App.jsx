2effd1bcccf56a7654929cb620bf5406f07492ae
IOspinaDev <ospigarcese@gmail.com>
2026-04-24 18:34:34 -0500

    cambios generales

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Impact from "./components/Impact";
 import Taller from "./components/Taller";
 import Contexto from "./components/Contexto";
 import Piangua from "./components/Piangua";
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
@@ -57,6 +58,7 @@ function App() {
       </section>
 
       <Footer />
+      <Analytics />
     </>
   );
 }
